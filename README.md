6386363faaa1cd0cde61ce54a6262194a8873065
valtony26 <kraizii666@yandex.ru>
2024-01-04 20:19:53 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## README 
+ 
+Добро пожаловать в репозиторий проекта! 
+ 
+### Контактная информация 
+ 
+- Почта: hdrezkahelper@yandex.ru 
+- Логин: hdrezkahelper 
+- Пароль: FaYAd7e5_n 
+- Телефон: 89887390873
+- smtp-пароль: tutmpsrroewtvjvd
+
+### Исходящие переменные
+
+- smtp_server: smtp.yandex.ru
+- smtp_port: 465 
+- sender_email: hdrezkahelper@yandex.ru 
+- sender_password: tutmpsrroewtvjvd
+- receiver_email: mirror@hdrezka.org
+
+### Входящие переменные
+
+- smtp_server: pop.yandex.ru
+- smtp_port: 465 
+- sender_email: hdrezkahelper@yandex.ru 
+- sender_password: tutmpsrroewtvjvd
+- receiver_email: mirror@hdrezka.org
--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import telebot
-import requests
+import smtplib
+import imaplib
 
 BOT_TOKEN = '6887127603:AAE9CYeUZCH4gv2YT62Hn0G_T0s90RwK8z0'
 bot = telebot.TeleBot(BOT_TOKEN)
 
+smtp_server = 'smtp.yandex.ru'
+smtp_port = 465
+sender_email = 'hdrezkahelper@yandex.ru'
+sender_password = 'tutmpsrroewtvjvd'
+receiver_email = 'mirror@hdrezka.org'
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
+    bot.send_message(message.chat.id,
+                     f'Привет, {message.from_user.first_name}! Я помогу тебе получить актуальную ссылку на HDRezka. Введите /send_email ')
 
+
+@bot.message_handler(commands=['send_email'])
+def send_email(message):
+    bot.reply_to(message, 'Введите текст письма')
+    bot.register_next_step_handler(message, process_email)
+
+def process_email(message):
+    email_text = message.text
+
+    try:
+        # Отправка письма
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, email_text)
+        server.quit()
+
+        bot.reply_to(message, 'Письмо успешно отправлено!')
+    except Exception as e:
+        bot.reply_to(message, f'Произошла ошибка при отправке письма')
 
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
-
